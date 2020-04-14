3aea42f5e8d0260d1943a42affcfa3414a1ae21f
hourai-dev <63674710+hourai-dev@users.noreply.github.com>
2020-04-14 17:00:06 -0500

    Added license (GPLv3)

--- ks_announce.py
+++ ks_announce.py
@@ -1,4 +1,22 @@
 """
+ks_announce.py
+Copyright (C) 2020 Night-Stalkers
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
 ks_announce
 Announces killstreaks, with custom messages, to the whole server every set number of kills
 after a set minimum. These values can be changed by setting "ks_announce_rate" and
